378788154329fc74510de06059e3f2d3e0423a92
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-17 08:35:20 -0600

    fix: change to staging

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // Initialize Apollo Client
 const client = new ApolloClient({
-  uri: "https://nbaql-production.up.railway.app/", // Replace with your GraphQL endpoint
+  uri: "https://nbaql-staging.up.railway.app/", // Replace with your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
